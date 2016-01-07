6cd3adeb2c6337d851aad62123ff1b6f1ff11a10
Michaberard <migelaens@gmail.com>
2016-01-07 14:35:37 +0100

    Added Login files

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+
+$host ="localhost";
+$username="root";
+$password="";
+$db="forum";
+
+mysql_connect($host, $username, $password) or die(mysql_error());
+mysql_select_db($db);
+
+
+
+
+
+
+?>
+
---
+++ index.php
@@ -0,0 +1,229 @@
+<?php session_start(); ?>
+<html>
+<title> ASROMAFAMILY </title>
+    <head>
+    <link href="CSS/ASF.css" rel="stylesheet" type="text/css">
+  
+
+<script src="JS/jquery-1.11.3.js"></script>
+
+    </head>
+    
+    
+<body>
+
+        <?php
+        if (!isset($_SESSION['uid']))
+        {
+            echo "<form action='login_parse.php' method='post'>
+            Username:<input type='text' name='username' />&nbsp;
+            Password:<input type='pasword' name='password'/>&nbsp;
+            <input type='submit' name='submit' value='Log In'/>";
+            
+        }
+        else 
+        {
+            echo "<p>You are logged in as".$_SESSION['username']."&bull;<a href='logout_parse.php'>Logout</a>";
+                    
+        }
+        ?>
+    
+    
+    
+    
+<div id="header">
+<h1> <img src="Pic/logo.jpg" alt="logo" align="center" style="width:100px;height:70px" > Welcome to the best AS Roma fan website! </h1>
+    </div>
+  <div id="navigation">  
+      <div id="nav">  
+    <ul >
+     <li><a href="Today.php" class="current">News</a>
+        <ul >
+                        <li><a href="Today.php">Today</a></li>
+                        <li><a href="Last_week.php">Last week</a></li>
+                        <li><a href="Last_Month.php">Last month</a></li>
+                        <li><a href="More.php">More...</a></li>
+        </ul>
+        </li>
+    <li><a href="Compet.html" >Competition</a>
+        <ul >
+                        <li><a href="SerieA.php">Serie A Tim</a></li>
+                        <li><a href="UCL.html">Campions League</a></li>
+                        <li><a href="ItalCup.php">Italian Cup</a></li>
+        </ul>
+        </li>
+        <li><a href="Multim.php" >Multimedia</a>
+        <ul >
+                        <li><a href="Video.php">Video</a></li>
+                        <li><a href="Gallery.php">Gallery</a></li>
+        </ul>
+        </li>
+        <li><a href="Store.php">Store</a></li>
+        <li><a href="About.php">About</a>
+        <ul >
+                        <li><a href="History.php">History</a></li>
+                        <li><a href="Team.php">Team List</a></li>
+                        <li><a href="Sponsor.php">Sponsor</a></li>
+        </ul>
+        </li>
+        <li><a href="Contact_us.php" >Contact</a>
+        <ul >
+                        <li><a href="Contact_us.php">Support</a></li>
+                        <li><a href="SocialMedia.php">Social Media</a></li>
+        </ul>
+        </li>
+    </ul>
+          <br class="clearF"/>
+  </div>
+</div>
+    <div id="content">
+    <img src="Pic/ASR.jpg" alt="photo" style="width:100%;height:100%">
+        <table class="table6">
+            <tr> 
+                <td colspan="2"><img src="Pic/Arti1.jpg" alt="article1" align="left" style="width:100%;height:200px"/> </td>
+    <td class="linkart"><a href="http://www.forzaroma.info/news-as-roma/dzeko-verso-il-rientro-con-la-bosnia-trigoria-recuperati-rudiger-e-falque/" target="_blank"> Dzeko close to his come back with Bosnia</a></td> 
+            </tr>
+                <tr> 
+    <td colspan="2"><img src="Pic/Arti2.jpg" alt="article1" align="left" style="width:100%;height:200px"/></td>
+    <td class="linkart"><a href="http://www.forzaroma.info/news-calcio/conte-la-qualificazione-ad-euro-2016-labbiamo-raggiunta-con-un-determito-gruppo-questo-conta/" target="_blank">Antonio Conte: "We have reached the qualification as a team."</a></td>
+                </tr>
+          <tr>  
+              <td colspan="2"><img src="Pic/Arti3.jpg" alt="article1" align="left" style="width:100%;height:200px"/></td>
+    <td class="linkart"><a href="http://www.forzaroma.info/news-as-roma/trigoria-il-programma-settimanale-degli-allenamenti-7/" target="_blank">Trigoria, the weekly training schedule</a></td>
+    </tr>
+      <tr>  
+          <td colspan="2"><img src="Pic/debouchy2.jpg" alt="article1" align="left" style="width:100%;height:200px"/></td>
+    <td class="linkart"><a href="http://www.forzaroma.info/news-as-roma/mercato-la-roma-pensa-a-debuchy-per-gennaio/" target="_blank">Transfer, Roma is interested in Debuchy for January.</a></td>
+    </tr>
+    <tr>
+        <td colspan="2"><img src="Pic/Totti.jpg" alt="article1" align="left" style="width:100%;height:200px"/></td>
+    <td class="linkart"><a href="http://www.espnfc.com/as-roma/story/2623534/francesco-totti-scores-300th-career-goal" target="_blank">Francesco Totti scores 300th career goal in Roma's 2-2 draw vs. Sassuolo</a></td>
+    </tr><span></span>
+        </table>
+<table class="table1">
+        <thead>
+        <tr>
+            <th scope="col">Pos</th>
+            <th scope="col">Squad</th>
+            <th scope="col">Pts</th>
+        </tr>
+        </thead>
+        <tbody>
+       
+            <td>1</td>
+            <td>Fiorentina</td>
+            <td>18</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Inter</td>
+            <td>16</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Lazio</td>
+            <td>15</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Romaa</td>
+            <td>14</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Torino</td>
+            <td>13</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Napoli</td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Chievo</td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Sassuolo</td>
+            <td>12</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>Sampdoria</td>
+            <td>11</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Atalanta</td>
+            <td>11</td>
+        </tr>
+        <tr>
+            <td>11</td>
+            <td>Milan</td>
+            <td>9</td>
+        </tr>
+        <tr>
+            <td>12</td>
+            <td>Juventus</td>
+            <td>8</td>
+        </tr>
+        <tr>
+            <td>13</td>
+            <td>Palermo</td>
+            <td>7</td>
+        </tr>
+        <tr>
+            <td>14</td>
+            <td>Udinese</td>
+            <td>7</td>
+        </tr>
+         <tr>
+            <td>15</td>
+            <td>Genoa</td>
+            <td>7</td>
+        </tr>
+        <tr>
+            <td>16</td>
+            <td>Empoli</td>
+            <td>7</td>
+        </tr>
+        <tr>
+            <td>17</td>
+            <td>Carpi</td>
+            <td>5</td>
+        </tr>
+        <tr>
+            <td>18</td>
+            <td>Verona</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>19</td>
+            <td>Frosinone</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>20</td>
+            <td>Bologna</td>
+            <td>3</td>
+        </tr>
+        </tbody><span></span>
+        </table>
+    
+        
+    </div>
+
+<div id="footer">
+ <footer>
+  <p>Build by: Michel Clerger</p>
+  <p>Contact Us: <a href="Pages/Contact_US.php" >
+  Send an email</a>.</p>
+</footer>     
+</div>
+
+<script src="JS/load.js"></script>
+
+</body>
+</html> 
