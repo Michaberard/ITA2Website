ca20e2badb6b97f1453c720b217938dbf370cf24
beam4014 <beam_sws@hotmail.com>
2016-01-14 14:45:26 +0200

    Change of table

--- Store/tblproduct.sql
+++ Store/tblproduct.sql
@@ -1,23 +1,21 @@
-CREATE TABLE IF NOT EXISTS `tblproduct` (
-  
-`id` int(8) NOT NULL AUTO_INCREMENT,
-  
-`name` varchar(255) NOT NULL,
-  
-`code` varchar(255) NOT NULL,
-  
-`image` text NOT NULL,
-  
-`price` double(10,2) NOT NULL,
-
-PRIMARY KEY (`id`),
-  
-UNIQUE KEY `product_code` (`code`)
-)
 
 
+CREATE TABLE `tblproduct` (
+  `ItemId` int(8) NOT NULL,
+  `ItemName` varchar(255) NOT NULL,
+  `ItemCode` varchar(255) NOT NULL,
+  `ItemImage` text NOT NULL,
+  `ItemPrice` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `tblproduct` (`id`, `name`, `code`, `image`, `price`) VALUES
+--
+-- Dumping data for table `tblproduct`
+--
+
+INSERT INTO `tblproduct` (`ItemId`, `ItemName`, `ItemCode`, `ItemImage`, `ItemPrice`) VALUES
 (1, 'Ball', 'Ball01', 'Pic/ball.jpg', 5.00),
 (2, 'Jersey', 'Jersey03', 'Pic/Jersey3.jpg', 15.00),
-(3, 'Scarf', 'Scarf01', 'Pic/Foulard.jpg', 2.00),(4, 'Jersey', 'Jersey02', 'Pic/Jersey2.jpg', 15.00),(5, 'Shorts', 'Shorts01', 'Pic/shorts.jpg', 4.00),(6, 'Hat', 'Hat01', 'Pic/Hat.jpg', 5.00;
+(3, 'Scarf', 'Scarf01', 'Pic/Foulard.jpg', 2.00),
+(4, 'Jersey', 'Jersey02', 'Pic/Jersey2.jpg', 15.00),
+(5, 'Shorts', 'Shorts01', 'Pic/shorts.jpg', 4.00),
+(6, 'Hat', 'Hat01', 'Pic/Hat.jpg', 5.00);
